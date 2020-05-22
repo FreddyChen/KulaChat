b010a7b3113e8b919d5feb054fd07a1803cdd382
FreddyChen <chenshichao@outlook.com>
2020-05-23 01:20:12 +0800

    调整项目架构

---
+++ app/src/main/java/com/freddy/kulachat/config/CConfig.java
@@ -0,0 +1,12 @@
+package com.freddy.kulachat.config;
+
+/**
+ * @author FreddyChen
+ * @name
+ * @date 2020/05/23 01:08
+ * @email chenshichao@outlook.com
+ * @github https://github.com/FreddyChen
+ * @describe
+ */
+public class CConfig {
+}
---
+++ app/src/main/java/com/freddy/kulachat/view/main/MainActivity.java
@@ -0,0 +1,7 @@
+package com.freddy.kulachat.view.main;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
